d76e6961b4737c56be8b70c248039cdb0ce1aa82
LucianoMazar <lmazaraojr@gmail.com>
2025-06-30 15:40:04 -0300

    Implementando a lógica de cart.mjs + função displayCart

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import createItem from "./services/item.mjs";
+import * as cartService from "./services/cart.mjs";
 
-const cart = [];
+const myCart = [];
 
 const item1 = await createItem("Produto 1", 20.99, 1);
 const item2 = await createItem("Produto 2", 15.99, 3);
 
-console.log(item2.subtotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+const total = await cartService.calculateTotal(myCart);
+console.log("Total do carrinho: " + total);
--- src/services/cart.mjs
+++ src/services/cart.mjs
@@ -1,19 +1,43 @@
 //Adicionar item ao carrinho
-async function addItem(userCart) {
-    
+async function addItem(userCart, item) {
+    userCart.push(item);
 }
 
 //Remover um item do carrinho
-async function removeItem(userCart, name) {
-    
+async function removeItem(userCart, item) {
+    const index = userCart.findIndex(cartItem => cartItem.name === item.name);
+    if (index !== -1) {
+        userCart[index].quantity -= 1;
+        if( userCart[index].quantity <= 0) {
+            userCart.splice(index, 1);
+        }
+    }
 }
 
 //Tirar o item do carrinho
-async function deleteItem(userCart, index) {
-
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex(item => item.name === name);
+    if (index !== -1) {
+        userCart.splice(index, 1);
+    } 
 }
 
 //Calcular o valor total do carrinho
 async function calculateTotal(userCart) {
+    return userCart.reduce((total, item) => total + item.subtotal(), 0);
+}
 
+async function displayCart(userCart) {
+    console.log("\nItens no carrinho de Compras:");
+    userCart.forEach((item, i) => {
+        console.log(`Item ${i + 1}: ${item.name}, Preço: ${item.price}, Quantidade: ${item.quantity}, Subtotal: ${item.subtotal()}`);
+    });
+}
+
+export {
+    addItem,
+    removeItem,
+    deleteItem,
+    displayCart,
+    calculateTotal
 }
