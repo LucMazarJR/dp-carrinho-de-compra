4b8dc943869c1dfc02969b3af27f357bfe1ea305
LucianoMazar <lmazaraojr@gmail.com>
2025-06-30 09:28:18 -0300

    Implementando a lógica de item.mjs + testes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-
+import createItem from "./services/item.mjs";
 
 const cart = [];
+
+const item1 = await createItem("Produto 1", 20.99, 1);
+const item2 = await createItem("Produto 2", 15.99, 3);
+
+console.log(item2.subtotal());
